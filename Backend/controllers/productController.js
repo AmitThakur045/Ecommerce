be4139654e939724d2afd5b638042a4e34e47b67
Amit Thakur <at7129652@gmail.com>
2022-01-28 21:41:29 +0530

    Created function to get all product details

--- Backend/controllers/productController.js
+++ Backend/controllers/productController.js
@@ -1,18 +1,33 @@
 const Product = require("../models/productModel");
 
-// Create Product
+// Create Product - Admit only admin
 exports.createProduct = async (req, res, next) => {
-    try {
-        const product = await Product.create(req.body);
-        res.status(201).json({
-            success: true,
-            product,
-        });
-    } catch (err) {
-        res.status(400).json({
-            success: false,
-            message: err.message,
-        });
-    }
+  try {
+    const product = await Product.create(req.body);
+    res.status(201).json({
+      success: true,
+      product,
+    });
+  } catch (err) {
+    res.status(400).json({
+      success: false,
+      message: err.message,
+    });
+  }
 };
 
+// Get All Products
+exports.getAllProducts = async (req, res) => {
+  try {
+    const products = await Product.find();
+    res.status(200).json({
+      success: true,
+      products,
+    });
+  } catch (err) {
+    res.status(400).json({
+      success: false,
+      message: err.message,
+    });
+  }
+};
--- Backend/server.js
+++ Backend/server.js
@@ -1,8 +1,12 @@
 const app = require("./app");
 const dotenv = require('dotenv');
+const connectDatabase = require("./config/database");
 
 // Config
 dotenv.config({ path: 'Backend/config/config.env' });
+
+// Connecting to database
+connectDatabase();
 
 app.listen(process.env.PORT, () => {
     console.log(`Server is listening on port ${process.env.PORT}`);
