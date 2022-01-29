12f8fcdbf640cabcff151d553c7ac93032369466
Amit Thakur <at7129652@gmail.com>
2022-01-29 21:31:54 +0530

    Implemented filter query for price and product name

--- Backend/utils/apifeatures.js
+++ Backend/utils/apifeatures.js
@@ -12,9 +12,22 @@ class ApiFeatures {
         },
     } : {}
 
-    console.log(keyword);
-    
     this.query = this.query.find({...keyword});
+    return this;
+  }
+
+  filter() {
+    // pass by value
+    const queryCopy = {...this.queryStr}; 
+    // remove the fields for category
+    const removeFields = ["keyword","page","limit"];
+    removeFields.forEach(key => delete queryCopy[key]);
+
+    let queryStr = JSON.stringify(queryCopy);
+    // adding $ sign to the beginning of the string to make it a valid regex
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
+
+    this.query = this.query.find(JSON.parse(queryStr));
     return this;
   }
 }
