c0284142d65a92d0b05469adaead39f1e626259b
Amit Thakur <at7129652@gmail.com>
2022-01-29 20:55:15 +0530

    Handling CastError

--- Backend/middleware/error.js
+++ Backend/middleware/error.js
@@ -4,6 +4,11 @@ module.exports = (err, req, res, next) => {
     err.statusCode = err.statusCode || 500;
     err.message = err.message || 'Internal Server Error';
 
+    // CastError : wrong mongodb id error
+    if(err.name === 'CastError') {
+        err = new ErrorHandler(`Resource not found with id ${err.value}`, 404);
+    }
+
     res.status(err.statusCode).json({
         success: false,
         message: err.message,
