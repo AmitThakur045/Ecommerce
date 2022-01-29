fd596055e47b0631d3bdf1deddcf6c294e2fb873
Amit Thakur <at7129652@gmail.com>
2022-01-29 21:15:35 +0530

    Implemented search query

--- Backend/controllers/productController.js
+++ Backend/controllers/productController.js
@@ -1,6 +1,7 @@
 const Product = require("../models/productModel");
 const ErrorHandler = require("../utils/errorHandler");
 const catchAsyncErrors = require("../middleware/catchAsyncError");
+const ApiFeatures = require("../utils/apifeatures");
 
 // Create Product - Admit only admin
 exports.createProduct = catchAsyncErrors(async (req, res, next) => {
@@ -14,7 +15,8 @@ exports.createProduct = catchAsyncErrors(async (req, res, next) => {
 
 // Get All Products
 exports.getAllProducts = catchAsyncErrors(async (req, res) => {
-  const products = await Product.find();
+  const apiFeature = new ApiFeatures(Product.find(), req.query).search();
+  const products = await apiFeature.query;
 
   res.status(200).json({
     success: true,
