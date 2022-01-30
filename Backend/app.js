dc2e4b935e9765eac967565a809b4e388a996974
Amit Thakur <at7129652@gmail.com>
2022-01-30 15:39:38 +0530

    Created register route and function

--- Backend/app.js
+++ Backend/app.js
@@ -7,8 +7,10 @@ app.use(express.json());
 
 // Routes Imports
 const product = require('./routes/productRoute');
+const user = require('./routes/userRoute');
 
 app.use("/api/v1", product);
+app.use("/api/v1", user);
 
 // Middleware Error Handler
 app.use(errorMiddleware);
---
+++ Backend/controllers/userController.js
@@ -0,0 +1,22 @@
+const ErrorHandler = require("../utils/errorHandler");
+const catchAsyncErrors = require("../middleware/catchAsyncError");
+const User = require("../models/userModels");
+
+// register user
+exports.registerUser = catchAsyncErrors(async (req, res, next) => {
+    const { name, email, password } = req.body;
+    const user = await User.create({ 
+        name, email, password,
+        avatar: {
+            public_id: 'This is sample public id',
+            url: 'This is sample url',
+        },
+    });
+    
+    const token = user.getJWTToken();
+
+    res.status(201).json({
+        success: true,
+        token,
+    });
+}); 
