006a2d57955bb9e27d3632f5247a0091a9e61f49
Amit Thakur <at7129652@gmail.com>
2022-01-30 16:03:24 +0530

    Created login route and function

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -20,3 +20,32 @@ exports.registerUser = catchAsyncErrors(async (req, res, next) => {
         token,
     });
 }); 
+
+// login User
+exports.loginUser = catchAsyncErrors(async (req, res, next) => {
+    const { email, password } = req.body;
+    // 1) Check if email and password exist
+    if (!email || !password) {
+        return next(new ErrorHandler("Please provide email and password", 400));
+    }
+    // 2) Check if user exists && password is correct
+    const user = await User.findOne({ email }).select("+password");
+
+    if(!user) {
+        return next(new ErrorHandler("Invalid email or password", 401));
+    }
+
+    const isMatch = await user.comparePassword(password, user.password);
+
+    if(!isMatch) {
+        return next(new ErrorHandler("Invalid email or password", 401));
+    }
+
+    const token = user.getJWTToken();
+
+    res.status(200).json({
+        success: true,
+        token,
+    });
+
+});
