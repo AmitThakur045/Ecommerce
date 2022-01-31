e84a2aadf35c16b062e9bf45b9c78618fbf31e73
Amit Thakur <at7129652@gmail.com>
2022-01-31 19:24:13 +0530

    Created route and function to generate and send password reset link

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -2,59 +2,103 @@ const ErrorHandler = require("../utils/errorHandler");
 const catchAsyncErrors = require("../middleware/catchAsyncError");
 const User = require("../models/userModels");
 const sendToken = require("../utils/jwtTokens");
+const sendEmail = require("../utils/sendEmail");
 
 // register user
 exports.registerUser = catchAsyncErrors(async (req, res, next) => {
-    const { name, email, password } = req.body;
-    const user = await User.create({ 
-        name, email, password,
-        avatar: {
-            public_id: 'This is sample public id',
-            url: 'This is sample url',
-        },
-    });
-    
-    const token = user.getJWTToken();
+  const { name, email, password } = req.body;
+  const user = await User.create({
+    name,
+    email,
+    password,
+    avatar: {
+      public_id: "This is sample public id",
+      url: "This is sample url",
+    },
+  });
 
-    sendToken(user, 201, res);
-}); 
+  const token = user.getJWTToken();
+
+  sendToken(user, 201, res);
+});
 
 // login User
 exports.loginUser = catchAsyncErrors(async (req, res, next) => {
-    const { email, password } = req.body;
-    // 1) Check if email and password exist
-    if (!email || !password) {
-        return next(new ErrorHandler("Please provide email and password", 400));
-    }
-    // 2) Check if user exists && password is correct
-    const user = await User.findOne({ email }).select("+password");
+  const { email, password } = req.body;
+  // 1) Check if email and password exist
+  if (!email || !password) {
+    return next(new ErrorHandler("Please provide email and password", 400));
+  }
+  // 2) Check if user exists && password is correct
+  const user = await User.findOne({ email }).select("+password");
 
-    if(!user) {
-        return next(new ErrorHandler("Invalid email or password", 401));
-    }
+  if (!user) {
+    return next(new ErrorHandler("Invalid email or password", 401));
+  }
 
-    const isMatch = await user.comparePassword(password, user.password);
+  const isMatch = await user.comparePassword(password, user.password);
 
-    if(!isMatch) {
-        return next(new ErrorHandler("Invalid email or password", 401));
-    }
+  if (!isMatch) {
+    return next(new ErrorHandler("Invalid email or password", 401));
+  }
 
-    const token = user.getJWTToken();
+  const token = user.getJWTToken();
 
-    sendToken(user, 200, res); // send token to the user
-
+  sendToken(user, 200, res); // send token to the user
 });
 
 // Logout User
 exports.logoutUser = catchAsyncErrors(async (req, res, next) => {
+  res.cookie("token", null, {
+    expires: new Date(Date.now()),
+    httpOnly: true,
+  });
 
-    res.cookie('token', null, {
-        expires: new Date(Date.now()),
-        httpOnly: true
+  res.status(200).json({
+    success: true,
+    message: "Logged out successfully",
+  });
+});
+
+// Forgot Password
+exports.forgotPassword = catchAsyncErrors(async (req, res, next) => {
+  const user = await User.findOne({ email: req.body.email });
+
+  if (!user) {
+    return next(new ErrorHandler("User not found", 404));
+  }
+
+  // Get resetPassword token
+  const resetToken = user.getResetPasswordToken();
+  // Save the token to the user
+  await user.save({ validateBeforeSave: false });
+
+  // Create reset url
+  const resetUrl = `${req.protocol}://${req.get(
+    "host"
+  )}/api/v1/password/reset/${resetToken}`;
+
+  const message = `Your password reset token is :- \n\n ${resetUrl} \n\n If you did not request this, please ignore this email and your password will remain unchanged.`;
+
+  try {
+
+    await sendEmail({
+        email: user.email,
+        subject: "Ecommerce Password Recovery",
+        message,
     });
 
     res.status(200).json({
         success: true,
-        message: 'Logged out successfully'
+        message: `Email sent to ${user.email} successfully`,
     });
+
+  } catch (error) {
+    user.resetPasswordToken = undefined;
+    user.resetPasswordExpire = undefined;
+
+    await user.save({ validateBeforeSave: false });
+
+    return next(new ErrorHandler(error.message, 500));
+  }
 });
--- Backend/models/userModels.js
+++ Backend/models/userModels.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const validator = require("validator");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const crypto = require("crypto");
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -40,24 +41,41 @@ const userSchema = new mongoose.Schema({
   resetPasswordExpire: Date,
 });
 
-userSchema.pre("save", async function(next) {
+userSchema.pre("save", async function (next) {
   // avoid multiple hashing of the same password
-  if(!this.isModified("password")) {
+  if (!this.isModified("password")) {
     next();
   }
   this.password = await bcrypt.hash(this.password, 10); // 10 is the number of rounds
 });
 
 // JWT token
-userSchema.methods.getJWTToken = function() {
+userSchema.methods.getJWTToken = function () {
   return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
     expiresIn: process.env.JWT_EXPIRES_IN,
   });
-}
+};
 
 // Compare password
-userSchema.methods.comparePassword = async function(enteredPassword) {
+userSchema.methods.comparePassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
-} 
+};
+
+// Generating Password Reset Token
+userSchema.methods.getResetPasswordToken = function () {
+  // Generate token
+  const resetToken = crypto.randomBytes(20).toString("hex");
+
+  // Hashing and adding resetPasswordToken to userSchema
+  this.resetPasswordToken = crypto
+    .createHash("sha256")
+    .update(resetToken)
+    .digest("hex");
+
+  // Setting resetPasswordExpire
+  this.resetPasswordExpire = Date.now() + 15 * 60 * 1000; // 15 minutes
+
+  return resetToken;
+};
 
 module.exports = mongoose.model("User", userSchema);
