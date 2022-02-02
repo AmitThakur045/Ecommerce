cbccfeff75441d594014137d270c32a20f6d7f73
Amit Thakur <at7129652@gmail.com>
2022-02-02 19:42:07 +0530

    added 2 more error handlers

--- Backend/middleware/error.js
+++ Backend/middleware/error.js
@@ -9,6 +9,21 @@ module.exports = (err, req, res, next) => {
         err = new ErrorHandler(`Resource not found with id ${err.value}`, 404);
     }
 
+    // Mongoose duplicate key error
+    if(err.code === 11000) {
+        err = new ErrorHandler(`Duplicate ${Object.keys(err.keyValue)} Entered`, 400);
+    }
+    
+    // Wrong JWT token
+    if(err.code === "JsonWebTokenError") {
+        err = new ErrorHandler(`Json Web Token is invalid, Try angain`, 400);
+    }
+    
+    // JWT expired error
+    if(err.code === "TokenExpiredError") {
+        err = new ErrorHandler(`Json Web Token is Expired, Try angain`, 400);
+    }
+    
     res.status(err.statusCode).json({
         success: false,
         message: err.message,
