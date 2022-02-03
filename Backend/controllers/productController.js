3c758cbdf13ce88d5233c479fd02296a32da9dc4
Amit Thakur <at7129652@gmail.com>
2022-02-03 21:22:21 +0530

    Added function and routes to add reviews

--- Backend/controllers/productController.js
+++ Backend/controllers/productController.js
@@ -81,3 +81,46 @@ exports.deleteProduct = catchAsyncErrors(async (req, res, next) => {
     message: "Product deleted successfully",
   });
 });
+
+// Create New Review or Update the review
+exports.createProductReview = catchAsyncErrors(async (req, res, next) => {
+
+  const { rating, comment, productId } = req.body;
+
+  const review = {
+    user: req.user.id,
+    name: req.user.name,
+    rating : Number(rating),
+    comment,
+  };
+
+  const product = await Product.findById(productId);
+
+  // If this product is reviewed by this user or not
+  const isReviewed = product.reviews.find(rev => rev.user.toString() === req.user.id); 
+
+  if(isReviewed) {
+    product.reviews.forEach(rev => {
+      if(rev.user.toString() === req.user.id) {
+        rev.rating = Number(rating);
+        rev.comment = comment;
+      }
+    });
+  } else {
+    product.reviews.push(review);
+    product.numOfReviews = product.reviews.length;
+  }
+
+  let avgRating = 0;
+  product.reviews.forEach(rev => {
+    avgRating += rev.rating;
+  });
+
+  product.ratings = avgRating / product.reviews.length;
+
+  await product.save({ validateBeforeSave: false });
+
+  res.status(200).json({
+    success: true,
+  });
+});
