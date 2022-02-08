e07782f19ff0ba63021e8263d9ceb06bb12aa9cd
Amit Thakur <at7129652@gmail.com>
2022-02-08 12:33:55 +0530

    Created function for admin to update order status, delet order

--- Backend/controllers/orderController.js
+++ Backend/controllers/orderController.js
@@ -58,3 +58,72 @@ exports.myOrder = catchAsyncErrors(async (req, res, next) => {
         orders
     });
 });
+
+// Get All Orders -- Admin
+exports.getAllOrders = catchAsyncErrors(async (req, res, next) => {
+    // find all the orders
+    const orders = await Order.find();
+
+    let totalAmount = 0;
+
+    // looping through the orders and adding the total amount
+    orders.forEach((order) => {
+        totalAmount += order.totalPrice;
+    });
+
+    res.status(200).json({
+        success: true,
+        totalAmount,
+        orders,
+    });
+});
+
+// Update Order Status -- Admin
+exports.updateOrder = catchAsyncErrors(async (req, res, next) => {
+    // find all the orders
+    const order = await Order.findById(req.params.id);
+
+    if(order.orderStatus === "Delivered") {
+        return next(new ErrorHandler("You have already delivered this order", 404));
+    }
+
+    order.orderItems.forEach(async (item) => {
+       await updateStock(item.product, item.quantity);
+    });
+
+    order.orderStatus = req.body.status;
+
+    if(req.body.status === "Delivered") {
+        order.deliveredAt = Date.now();
+    }
+
+    await order.save({ validateBeforeSave: false });
+
+    res.status(200).json({
+        success: true,
+    });
+});
+
+async function updateStock (id, quantity) {
+    const product = await Product.findById(id);
+
+    product.Stock -= quantity;
+
+    await product.save({ validateBeforeSave: false });
+}
+
+// delete Orders -- Admin
+exports.deleteOrder = catchAsyncErrors(async (req, res, next) => {
+    // find the order with given id
+    const order = await Order.findById(req.params.id);
+
+    if(!order) {
+        next(new ErrorHandler("Order not found with this Id", 404));
+    }
+
+    await order.remove();
+
+    res.status(200).json({
+        success: true,
+    });
+});
