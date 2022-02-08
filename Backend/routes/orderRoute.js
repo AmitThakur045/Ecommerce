56941873863d922b6d94bb781161ab3f437578f0
Amit Thakur <at7129652@gmail.com>
2022-02-08 12:33:37 +0530

    Added routes for admin to update order status, delet order

--- Backend/routes/orderRoute.js
+++ Backend/routes/orderRoute.js
@@ -1,5 +1,12 @@
-const express = require('express');
-const { newOrder, getSingleOrder, myOrder } = require('../controllers/orderController');
+const express = require("express");
+const {
+  newOrder,
+  getSingleOrder,
+  myOrder,
+  updateOrder,
+  getAllOrders,
+  deleteOrder,
+} = require("../controllers/orderController");
 const router = express.Router();
 
 const { isAuthenticatedUser, authorizeRoles } = require("../middleware/auth");
@@ -9,5 +16,14 @@ router.route("/order/new").post(isAuthenticatedUser, newOrder);
 router.route("/order/:id").get(isAuthenticatedUser, getSingleOrder);
 
 router.route("/orders/me").get(isAuthenticatedUser, myOrder);
+
+router
+  .route("/admin/orders")
+  .get(isAuthenticatedUser, authorizeRoles("admin"), getAllOrders);
+
+router
+  .route("/admin/order/:id")
+  .put(isAuthenticatedUser, authorizeRoles("admin"), updateOrder)
+  .delete(isAuthenticatedUser, authorizeRoles("admin"), deleteOrder);
 
 module.exports = router;
