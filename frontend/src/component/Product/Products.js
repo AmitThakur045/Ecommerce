93486831a562d0aea0c71c9b21bffe948728ae90
Amit Thakur <at7129652@gmail.com>
2022-02-24 20:59:59 +0530

    Added filter using rating and categories

--- frontend/src/component/Product/Products.js
+++ frontend/src/component/Product/Products.js
@@ -8,11 +8,23 @@ import Slider from "@material-ui/core/Slider";
 import Typography from "@material-ui/core/Typography";
 import "./Products.css";
 
+const categories = [
+  "Laptop",
+  "Footware",
+  "Mobile",
+  "Tops",
+  "Attire",
+  "Camera",
+  "SmartPhone",
+]
+
 const Products = ({ match }) => {
   const dispatch = useDispatch();
 
   const [currentPage, setCurrentPage] = useState(1);
   const [price, setPrice] = useState([0, 25000]);
+  const [category, setCategory] = useState("");
+  const [ratings, setRatings] = useState(0);
 
   const {
     products,
@@ -35,8 +47,8 @@ const Products = ({ match }) => {
   };
 
   useEffect(() => {
-    dispatch(getProduct(keyword, currentPage, price));
-  }, [dispatch, keyword, currentPage, price]);
+    dispatch(getProduct(keyword, currentPage, price, category, ratings));
+  }, [dispatch, keyword, currentPage, price, category, ratings]);
 
   let count = filteredProductsCount;
 
@@ -64,6 +76,31 @@ const Products = ({ match }) => {
               min={0}
               max={25000}
             />
+
+            <Typography>Categories</Typography>
+            <ul className="categoryBox">
+              {categories.map((category) => (
+                <li
+                  className="category-link"
+                  key={category}
+                  onClick={() => setCategory(category)}
+                >
+                  {category}
+                </li>
+              ))}
+            </ul>
+
+            <fieldset>
+              <Typography component="legend">Ratings Above</Typography>
+              <Slider
+                value={ratings}
+                onChange={(e, newRating) => setRatings(newRating)}
+                aria-labelledby="discrete-slider"
+                min={0}
+                max={5}
+                valueLabelDisplay="auto"
+              />
+            </fieldset>
           </div>
 
           {resultPerPage < count && (
